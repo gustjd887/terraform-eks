1d61648c63f97a99290992a19d9548625859e4d3
gustjd887 <gustjd887@gmail.com>
2023-06-03 12:15:07 +0900

    커스텀 시작 템플릿 사용하지 않도록 변경

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ module "eks" {
   subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.private_subnets
 
-  node_security_group_id = module.security-group.security_group_id
+  # node_security_group_id = module.security-group.security_group_id
 
   eks_managed_node_groups = {
     onboarding-eks-node = {
@@ -67,6 +67,8 @@ module "eks" {
 
       instance_types = ["t3.large"]
       capacity_type  = "SPOT"
+
+      use_custom_launch_template = false
 
       remote_access = {
         ec2_ssh_key               = "kuberix-lab"
@@ -80,6 +82,58 @@ module "eks" {
     Terraform   = "true"
   }
 }
+
+# module "eks_managed_node_group" {
+#   source = "terraform-aws-modules/eks/aws//modules/eks-managed-node-group"
+
+#   name            = "onboarding-eks-node"
+#   cluster_name    = "onboarding"
+#   cluster_version = "1.26"
+
+#   subnet_ids = module.vpc.private_subnets
+
+#   // The following variables are necessary if you decide to use the module outside of the parent EKS module context.
+#   // Without it, the security groups of the nodes are empty and thus won't join the cluster.
+#   cluster_primary_security_group_id = module.eks.cluster_primary_security_group_id
+#   vpc_security_group_ids            = [module.eks.node_security_group_id]
+
+#   // Note: `disk_size`, and `remote_access` can only be set when using the EKS managed node group default launch template
+#   // This module defaults to providing a custom launch template to allow for custom security groups, tag propagation, etc.
+#   // use_custom_launch_template = false
+#   // disk_size = 50
+#   //
+#   //  # Remote access cannot be specified with a launch template
+#   remote_access = {
+#     ec2_ssh_key               = "kuberix-lab"
+#     source_security_group_ids = [module.security-group.security_group_id]
+#   }
+
+#   min_size     = 1
+#   max_size     = 3
+#   desired_size = 3
+
+#   instance_types = ["t3.large"]
+#   capacity_type  = "SPOT"
+
+#   labels = {
+#     Environment = "test"
+#     GithubRepo  = "terraform-aws-eks"
+#     GithubOrg   = "terraform-aws-modules"
+#   }
+
+#   # taints = {
+#   #   dedicated = {
+#   #     key    = "dedicated"
+#   #     value  = "gpuGroup"
+#   #     effect = "NO_SCHEDULE"
+#   #   }
+#   # }
+
+#   tags = {
+#     Environment = "dev"
+#     Terraform   = "true"
+#   }
+# }
 
 module "ec2-instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
